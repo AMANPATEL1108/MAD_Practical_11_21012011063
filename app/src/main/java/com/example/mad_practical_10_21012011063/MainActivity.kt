25d9e1433748594ad9059a1ae3a0fc4de862c822
student <student@gnu.ac.in>
2023-11-02 08:19:44 +0530

    ok

--- app/src/main/java/com/example/mad_practical_10_21012011063/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011063/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.mad_practical_11_21012011063
-
-
-import android.app.Person
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mad_practical_10_21012011063.DatabaseHelper
+import com.example.mad_practical_11_21012011063.Contact
+import com.example.mad_practical_11_21012011063.ContactAdapter
+import com.example.mad_practical_11_21012011063.HttpRequest
+import com.example.mad_practical_11_21012011063.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -16,58 +21,86 @@ import org.json.JSONException
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView : RecyclerView
+    lateinit var databaseHelper: DatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fav=findViewById<FloatingActionButton>(R.id.fav1)
-        fav.setOnClickListener {
-            sendDatatoListview()
-//            Intent(this,MapsActivity::class.java).apply { startActivity(this) }
+
+        databaseHelper = DatabaseHelper(this)
+        var toolBar : Toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolBar)
+
+        val fetchBtn : FloatingActionButton = findViewById(R.id.btnSwap)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        fetchBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val data = HttpRequest().makeServiceCall(
+                        "https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
+                        "rbn0rerl1k0d3mcwgw7dva2xuwk780z1hxvyvrb1"
+                    )
+                    withContext(Dispatchers.Main) {
+                        try {
+                            if(data != null)
+                            {
+                                runOnUiThread{getPersonDetailsFromJson(data)}
+                            }
+                        }
+                        catch (e: Exception)
+                        {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+                catch (e: Exception)
+                {
+                    e.printStackTrace()
+                }
+            }
         }
     }
 
-    private fun getPersonDetailsFromJson(sJson: String?) {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_button1 -> {
+                Toast.makeText(this@MainActivity, "Clicked on item at menu!", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_button2 -> {
+                var personList: ArrayList<Contact> = databaseHelper.getAllPersons()
+                recyclerView.layoutManager = LinearLayoutManager(this)
+                recyclerView.adapter = ContactAdapter(this, personList)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun getPersonDetailsFromJson(sJson: String?)
+    {
         val personList = ArrayList<Contact>()
         try {
             val jsonArray = JSONArray(sJson)
-            for (i in 0 until jsonArray.length()) {
+            for(i in 0 until jsonArray.length())
+            {
                 val jsonObject = jsonArray[i] as JSONObject
                 val person = Contact(jsonObject)
                 personList.add(person)
             }
-            val personListView=findViewById<ListView>(R.id.listview1)
-            personListView.adapter = ContactAdapter(this, personList)
-        } catch (ee: JSONException) {
-            ee.printStackTrace()
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.adapter =ContactAdapter(this, personList)
         }
-    }
-    fun sendDatatoListview(){
-//        val personListView=findViewById<ListView>(R.id.listview1)
-//        val personList= arrayListOf<Contact>(
-//            Contact("1","SRK","Abc@gmail.com","54123697871","Ganpat University",20.5937,78.9629),
-//            Contact("2","VP","Xyz@gmail.com","456476845484","Ganpat University",20.5937,78.9629),
-//            Contact("3","RP","Rhp@gmail.com","41756567457","Ganpat University",20.5937,78.9629),
-//            Contact("4","SV","Sjv@gmail.com","54587545847","Ganpat University",20.5937,78.9629),
-//
-//
-//        )
-//        personListView.adapter=ContactAdapter(this,personList)
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val data = HttpRequest().makeServiceCall(
-                    "https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
-                    "rbn0rerl1k0d3mcwgw7dva2xuwk780z1hxvyvrb1")
-                withContext(Dispatchers.Main) {
-                    try {
-                        if(data != null)
-                            runOnUiThread{getPersonDetailsFromJson(data)}
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+        catch (e: JSONException)
+        {
+            e.printStackTrace()
         }
     }
 }
